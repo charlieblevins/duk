67b4f74be48b6ec48815afa5b33ca9c186c6c8b6
Charlie Blevins <blevins.charlie@gmail.com>
2016-05-01 10:09:19 -0400

    Adding gitignore

---
+++ Duck/InfoWindowView.swift
@@ -0,0 +1,32 @@
+//
+//  InfoWindowView.swift
+//  Duck
+//
+//  Created by Charlie Blevins on 4/30/16.
+//  Copyright © 2016 Charlie Blevins. All rights reserved.
+//
+
+import UIKit
+
+class InfoWindowView: UIView {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var moreDetailBtn: UIButton!
+    @IBOutlet weak var tags: UILabel!
+
+    class func instanceFromNib() -> InfoWindowView {
+        return UINib(nibName: "InfoWindow", bundle: nil).instantiateWithOwner(nil, options: nil)[0] as! InfoWindowView
+    }
+    
+    override func didMoveToSuperview() {
+        superview?.autoresizesSubviews = false;
+    }
+    
+    /*
+     // Only override drawRect: if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func drawRect(rect: CGRect) {
+     // Drawing code
+     }
+     */
+}
