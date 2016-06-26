aff280d480b967f31496843384e9310aa25a6c31
Charlie Blevins <blevins.charlie@gmail.com>
2016-06-26 16:10:24 -0400

    receiving messages from web page

--- Duck/AccountViewController.swift
+++ Duck/AccountViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import WebKit
 
-class AccountViewController: UIViewController {
-
-    @IBOutlet weak var webView: UIWebView!
+class AccountViewController: UIViewController, WKScriptMessageHandler {
+    
+    @IBOutlet var containerView: UIView! = nil
+    
+    var webView: WKWebView?
+    
+    override func loadView() {
+        super.loadView()
+        
+        
+        let config = WKWebViewConfiguration()
+        let controller = WKUserContentController()
+        controller.addScriptMessageHandler(self, name: "signInClicked")
+        config.userContentController = controller
+        self.webView = WKWebView(frame: self.view.frame, configuration: config)
+        self.view = self.webView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("account view")
+
         
         let url = NSURL(string: "http://dukapp.io")
         let request = NSURLRequest(URL: url!)
-        webView.loadRequest(request)
+        self.webView!.loadRequest(request)
+    }
+    
+    func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
+        print("RECIEVED MESSAGE: \(message)")
+        
+        if let data = message.body as? NSDictionary {
+            
+            // Temporarily store username and password
+            let creds = Credentials(email: data["username"] as! String, password: data["password"] as! String)
+        } else {
+            print("Unrecognized message format")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
