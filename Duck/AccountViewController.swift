391b277ad2979561b59f408c0a671f926f1fff69
Charlie Blevins <blevins.charlie@gmail.com>
2016-06-27 08:58:34 -0400

    More logic for detecting successful login through web view

--- Duck/AccountViewController.swift
+++ Duck/AccountViewController.swift
@@ -9,22 +9,35 @@
 import UIKit
 import WebKit
 
-class AccountViewController: UIViewController, WKScriptMessageHandler {
+class AccountViewController: UIViewController, WKScriptMessageHandler, WKNavigationDelegate {
     
     @IBOutlet var containerView: UIView! = nil
     
     var webView: WKWebView?
     
+    // Store credentials when submitted (before login success)
+    var tempCredentials: Credentials? = nil
+    
+    // Store successful credentials
+    var credentials: Credentials? = nil
+    
     override func loadView() {
         super.loadView()
         
-        
-        let config = WKWebViewConfiguration()
+        // Controller
         let controller = WKUserContentController()
         controller.addScriptMessageHandler(self, name: "signInClicked")
+        
+        // Configuration
+        let config = WKWebViewConfiguration()
         config.userContentController = controller
+        
+        // WK Webview
         self.webView = WKWebView(frame: self.view.frame, configuration: config)
         self.view = self.webView
+        
+        // Handle navigation delegate methods
+        self.webView?.navigationDelegate = self
     }
     
     override func viewDidLoad() {
@@ -38,17 +51,62 @@ class AccountViewController: UIViewController, WKScriptMessageHandler {
         self.webView!.loadRequest(request)
     }
     
+    // Receive message from page script
     func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
-        print("RECIEVED MESSAGE: \(message)")
         
         if let data = message.body as? NSDictionary {
+            print("Data received: \(data)")
             
-            // Temporarily store username and password
-            let creds = Credentials(email: data["username"] as! String, password: data["password"] as! String)
+            self.handleScriptMessage(data)
         } else {
             print("Unrecognized message format")
         }
         
+    }
+    
+    // Receive script data and act accordingly
+    func handleScriptMessage(data: NSDictionary) {
+        
+        switch data["action"] as! String {
+        
+        case "loginAttempt":
+            
+            // Temporarily store username and password
+            self.tempCredentials = Credentials(email: data["username"] as! String, password: data["password"] as! String)
+            break;
+        
+        default:
+            print("Could not handle script action: \(data["action"])")
+        }
+    }
+
+    /**
+     * Access the navigation response data to determine login success/fail
+     */
+    func webView(webView: WKWebView, decidePolicyForNavigationResponse navigationResponse: WKNavigationResponse, decisionHandler: (WKNavigationResponsePolicy) -> Void) {
+        
+        let status = (navigationResponse.response as! NSHTTPURLResponse).statusCode
+        
+        print(status)
+        
+        // If a 200 status AND /home is loaded AND tempCredentials are stored: assume successful login
+        if status == 200 && webView.URL?.absoluteString.rangeOfString("dukapp.io/home") != nil && self.tempCredentials != nil {
+            
+            // Save credentials in core data
+            self.tempCredentials!.save()
+            
+            // Save as permanenet
+            self.credentials = self.tempCredentials
+            
+            // Remove temp
+            self.tempCredentials = nil
+            
+        } else {
+            self.tempCredentials = nil
+        }
+        
+        // Always allow load
+        decisionHandler(.Allow)
     }
 
     override func didReceiveMemoryWarning() {
--- Duck/CredentialModel.swift
+++ Duck/CredentialModel.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
 
 struct Credentials {
     let email: String
@@ -32,6 +34,32 @@ struct Credentials {
             self.password = data[0].valueForKey("password") as! String
         } else {
             return nil
+        }
+    }
+    
+    // Save login/pass in core data
+    // TODO: Do not allow this function to save multiple
+    func save () {
+        
+        Util.deleteCoreDataForEntity("Login")
+        
+        // 1. Get managed object context
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        
+        // 2. Create new object as marker entity
+        let entity = NSEntityDescription.entityForName("Login", inManagedObjectContext:managedContext)
+        let login = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        // 3. Add username and password
+        login.setValue(self.email, forKey: "email")
+        login.setValue(self.password, forKey: "password")
+        
+        // 4. Save the marker object
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Could not save login: \(error), \(error.userInfo)")
         }
     }
 }
