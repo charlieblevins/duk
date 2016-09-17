e0d931b084427efd32c0c53b313d5589e05b0037
Charlie Blevins <blevins.charlie@gmail.com>
2016-09-17 18:45:59 -0400

    add search results and close x images

---
+++ Duck/SearchResultsViewController.swift
@@ -0,0 +1,57 @@
+//
+//  SearchResultsViewController.swift
+//  Duck
+//
+//  Created by Charlie Blevins on 9/7/16.
+//  Copyright © 2016 Charlie Blevins. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    var parentController: MapViewController
+    
+    init(_ parent: MapViewController) {
+        self.parentController = parent
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) is not allowed")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setMessage (message: String) {
+        resultsLabel.text = message
+    }
+    
+    @IBAction func closeTapped(sender: UIButton) {
+        self.view.removeFromSuperview()
+        self.removeFromParentViewController()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
